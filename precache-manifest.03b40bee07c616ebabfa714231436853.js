100e5565206fe9b519da92574af698049fac8f9c
Ramesh Vijayaraghavan <rvramesh@gmail.com>
2020-04-09 21:38:39 +0530

    Initial build subtree commit

---
+++ precache-manifest.03b40bee07c616ebabfa714231436853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d438fd635b034929d0ac7014789eb1dc",
+    "url": "/window-handling-react/index.html"
+  },
+  {
+    "revision": "787341917bc8b16eb4de",
+    "url": "/window-handling-react/static/css/main.0c5ae43b.chunk.css"
+  },
+  {
+    "revision": "fd9d0c7333e8556866db",
+    "url": "/window-handling-react/static/js/2.6a58698f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/window-handling-react/static/js/2.6a58698f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "787341917bc8b16eb4de",
+    "url": "/window-handling-react/static/js/main.830c2c43.chunk.js"
+  },
+  {
+    "revision": "296ef3271bfb57e77cc0",
+    "url": "/window-handling-react/static/js/runtime-main.abca1d21.js"
+  }
+]);
