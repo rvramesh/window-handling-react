1d1e8d38de768aef513468470eadb81e3a215cf2
Ramesh Vijayaraghavan <rvramesh@gmail.com>
2020-04-09 21:29:06 +0530

    Added Links

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import About from "./About/About";
+import Links from "./LinkExample/Links";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/links">Links</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/links">
+            <Links />
+          </Route>
+          <Route path="/">
+            <About />
+          </Route>
+          
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/LinkExample/Links.tsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import "./Links.css";
+
+export default function () {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [link, setLink] = useState("");
+  const [windowOpen, setWindowOpen] = useState(false);
+  useEffect(() => {
+    const winFeatures =
+      "screenX=0,screenY=0,top=0,left=0,scrollbars";
+    const winName = "window";
+    const winReplace = true;
+    const winObj =
+      windowOpen && link
+        ? window.open(link, winName, winFeatures, winReplace)
+        : null;
+    window.onunload = function () {
+      if (!winObj?.closed) {
+        winObj?.close();
+      }
+    };
+    return () => {
+      winObj?.close();
+      window.onunload = null;
+    };
+  }, [windowOpen, link]);
+  return (
+    <React.Fragment>
+      <h2>Links!</h2>
+      <div className="container">
+        <div className="input">
+          <form
+            onSubmit={(ev) => {
+              setLink(
+                `https://www.google.com/search?q=${encodeURI(searchTerm)}`
+              );
+              setWindowOpen(false);
+              ev.preventDefault();
+            }}
+          >
+            <label>
+              Name:
+              <input
+                type="text"
+                value={searchTerm}
+                onChange={(event) => {
+                  setSearchTerm(event.target.value);
+                  setLink("");
+                }}
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+          {link && (
+            <button
+              onClick={() => {
+                setWindowOpen(true);
+              }}
+              className="link"
+            >
+              Link
+            </button>
+          )}
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
