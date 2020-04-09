a31daf6d8e2a1a0d67071e55e87dd67538537d72
Ramesh Vijayaraghavan <rvramesh@gmail.com>
2020-04-09 21:55:43 +0530

    build subtree commit

---
+++ precache-manifest.6b4eb4418dcf1c3c130cf1d0e0831098.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c66b340970659ac813c727cee768a9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fba24bde20a9dc155a91",
+    "url": "./static/css/main.0c5ae43b.chunk.css"
+  },
+  {
+    "revision": "fd9d0c7333e8556866db",
+    "url": "./static/js/2.6a58698f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.6a58698f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fba24bde20a9dc155a91",
+    "url": "./static/js/main.f711b3cc.chunk.js"
+  },
+  {
+    "revision": "bf45ae1000ce3b911563",
+    "url": "./static/js/runtime-main.28b4ab23.js"
+  }
+]);
